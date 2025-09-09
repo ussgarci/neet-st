27cb95595ecd49c92eb4eaa287f52fbcaeb750e7
sgarcia <stevensantosgarcia@gmail.com>
2025-09-08 21:12:38 -0500

    Add tests

--- src/BinarySearch/BinarySearch.hs
+++ src/BinarySearch/BinarySearch.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE MultiWayIf #-}
 
-module BinarySearch.BinarySearch () 
+module BinarySearch.BinarySearch (search)
 where
 
 import Control.Monad (forM_)
@@ -13,20 +13,21 @@ import qualified Data.Vector as V
 search :: Vector Int -> Int -> Int
 search nums target = runST $ do
     startRef <- newSTRef 0
-    endRef   <- newSTRef (V.length nums - 1)
+    endRef <- newSTRef (V.length nums - 1)
     let loop = do
-          i <- readSTRef startRef
-          j <- readSTRef endRef
-          if i > j
-            then pure (-1)
-            else do
-              let mid = (i + j) `div` 2
-              let val = nums V.! mid
-              if | val == target -> pure mid
-                 | val > target  -> do
-                    writeSTRef endRef (mid - 1)
-                    loop
-                 | otherwise -> do
-                    writeSTRef startRef (mid + 1)
-                    loop
+            i <- readSTRef startRef
+            j <- readSTRef endRef
+            if i > j
+                then pure (-1)
+                else do
+                    let mid = (i + j) `div` 2
+                    let val = nums V.! mid
+                    if
+                            | val == target -> pure mid
+                            | val > target -> do
+                                writeSTRef endRef (mid - 1)
+                                loop
+                            | otherwise -> do
+                                writeSTRef startRef (mid + 1)
+                                loop
     loop
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,29 @@
 module Main (main) where
 
+import qualified Data.Vector as V
+import Test.Tasty
+import Test.Tasty.HUnit
+
+-- Import the function to be tested
+import BinarySearch.BinarySearch (search)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain tests
+
+tests :: TestTree
+tests =
+    testGroup
+        "BinarySearch"
+        [ testCase "Case 1" $
+            let nums = V.fromList [-1, 0, 2, 4, 6, 8]
+                target = 4
+                expected = 3
+                actual = search nums target
+             in assertEqual "Should return the correct index for an existing element" expected actual
+        , testCase "Case 2" $
+            let nums = V.fromList [-1, 0, 2, 4, 6, 8]
+                target = 3
+                expected = -1
+                actual = search nums target
+             in assertEqual "Should return -1 for a non-existent element" expected actual
+        ]
